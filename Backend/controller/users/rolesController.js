680cf6507a185b96ddae828c489c8d272b26788e
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-01-08 06:23:05 +0700

    Create module : users.roles

---
+++ Backend/controller/users/rolesController.js
@@ -0,0 +1,55 @@
+import models, { sequelize } from "../../models/init-models"
+
+const dateTime = new Date()
+
+const findAllRows = async (req, res) => {
+    const result = await models.roles.findAll();
+    return res.send(result);
+}
+
+
+const findAllRowsById = async (req, res) => {
+    const result = await models.roles.findByPk(req.params.id);
+    return res.send(result);
+}
+
+const createRoles = async (req, res) => {
+    await models.roles.create({
+        role_name: req.body.role_name
+    }).then(result => {
+        return res.send("Data berhasil ditambahkan")
+    }).catch(err => {
+        return res.send("Data gagal ditambahkan\n" + err)
+    });
+}
+
+const updateRoles = async (req, res) => {
+    const result = await models.roles.update({
+        role_name: req.body.role_name
+    }, {
+        returning: true,
+        where: { role_id: req.params.id }
+    })
+
+    return res.send(result)
+}
+
+const deleteRoles = async (req, res) => {
+    await models.roles.destroy({
+        where: { role_id: req.params.id }
+    }).then(result => {
+        return res.send({ message: "Data berhasil dihapus" })
+    }).catch(err => {
+        return res.send({ message: "Data gagal dihapus\n" + err })
+    });
+
+}
+
+
+export default {
+    findAllRows,
+    findAllRowsById,
+    createRoles,
+    updateRoles,
+    deleteRoles
+}
--- Backend/routes/indexRoutes.js
+++ Backend/routes/indexRoutes.js
@@ -1,6 +1,6 @@
-import {Router} from "express";
+import { Router } from "express";
 import jobRoleController from "../controller/humanresource/jobRoleController"
-
+import rolesController from "../controller/users/rolesController";
 
 const router = new Router();
 /* JOB_ROLE */
@@ -10,6 +10,12 @@ router.post("/jobrole/", jobRoleController.createJobRole)
 router.put("/jobrole/:id", jobRoleController.updateJobRole)
 router.delete("/jobrole/:id", jobRoleController.deleteJobRole)
 
+/* USERS.ROLES */
+router.get("/roles", rolesController.findAllRows)
+router.get("/roles/:id", rolesController.findAllRowsById)
+router.post("/roles/", rolesController.createRoles)
+router.put("/roles/:id", rolesController.updateRoles)
+router.delete("/roles/:id", rolesController.deleteRoles)
 
 
 
