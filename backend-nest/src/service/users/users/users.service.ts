e0d34153b7edb3f74e9a7c11a29913eddcfa13de
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-01-13 11:07:49 +0700

    Update users.service.ts

--- backend-nest/src/service/users/users/users.service.ts
+++ backend-nest/src/service/users/users/users.service.ts
@@ -1,15 +1,24 @@
-import { HttpStatus, Injectable, Req, Res } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable, Req, Res } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm'
-import { Repository } from 'typeorm';  
+import { Repository, getConnection } from 'typeorm';  
 import { Users } from 'entities/Users';
 import { UserRoles } from 'entities/UserRoles';
-
+import { UserPassword } from 'entities/UserPassword';
+import * as bcrypt from 'bcrypt';
+import { UserBonusPoints } from 'entities/UserBonusPoints';
+import { UserMembers } from 'entities/UserMembers';
+import { UserProfiles } from 'entities/UserProfiles';
 
 @Injectable()
 export class UsersService {
     constructor(
         @InjectRepository(Users)
-        private userRepository: Repository<Users>
+        private userRepository: Repository<Users>,
+        private userRolesRepository: Repository<UserRoles>,
+        private userPasswordRepository: Repository<UserPassword>,
+        private userBonusPointsRepository: Repository<UserBonusPoints>,
+        private userMembersRepository: Repository<UserMembers>,
+        private userProfilesRepository: Repository<UserProfiles>
     ) { }
         
     async findAllUsers(req:any, res:any): Promise<any>{
@@ -116,5 +125,115 @@ export class UsersService {
                 });
             });
     }
-    
+
+    async createUser(
+        dataUsers: Users,
+        dataUserRoles: UserRoles,
+        dataUserPassword: UserPassword,
+        dataUserBonusPoints: UserBonusPoints,
+        dataUserMembers: UserMembers,
+        dataUserProfiles: UserProfiles,
+        req: any, res: any
+    ): Promise<any> {
+
+        const connection = getConnection();
+        const queryRunner = connection.createQueryRunner();
+
+        let now = Date();
+        let idUser: number;
+
+        // save data users.users
+        const saveUsers = await this.userRepository.save({
+            userFullName: dataUsers.userFullName,
+            userType: dataUsers.userType,
+            userCompanyName: dataUsers.userCompanyName,
+            userEmail: dataUsers.userEmail,
+            userPhoneNumber: dataUsers.userPhoneNumber,
+            userModifiedDate: now // create date default : now
+        }).then((result: any) => {
+            if (result) {
+                idUser = result.id;
+            } else {
+                throw new BadRequestException("Data insert failed");
+            }
+        }).catch((err: any) => {
+            return err;
+        });
+
+        // save data users.user_roles
+        const saveUserRoles = await this.userRolesRepository.save({
+            usroUserId: idUser, // idUser : get by resultSaveUsers
+            usroRoleId: dataUserRoles.usroRole
+        }).then((result: any) => {
+            if (!result) {
+                throw new BadRequestException("Data insert failed");
+            }
+        }).catch((err: any) => {
+            return err;
+        });
+
+        // save data users.user_password
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(dataUserPassword.uspaPasswordhash, salt);
+        const saveUserPassword = await this.userPasswordRepository.save({
+            uspaUserId: idUser, // idUser : get by resultSaveUsers
+            uspaPasswordhash: hashedPassword,
+            uspaPasswordsalt: 'bcrypt'
+        }).then((result:any) => {
+            if (!result) {
+                throw new BadRequestException("Data insert failed");
+            }
+        }).catch((err: any) => {
+            return err;
+        });
+
+        // save data users.user_bonus_points
+        const saveUserBonusPoints = await this.userBonusPointsRepository.save({
+            ubpoUserId: idUser, // idUser : get by resultSaveUsers
+            ubpoTotalPoints: dataUserBonusPoints.ubpoTotalPoints,
+            ubpoBonusType: dataUserBonusPoints.ubpoBonusType,
+            ubpoCreateOn: now // create date default : now
+        }).then((result: any) => {
+            if (!result) {
+                throw new BadRequestException("Data insert failed");
+            }
+        }).catch((err: any) => {
+            return err;
+        });
+
+        // save data users.user_members
+        const saveUserMembers = await this.userMembersRepository.save({
+            usmeUserId: idUser, // idUser : get by resultSaveUsers
+            usmeMembName: dataUserMembers.usmeMembName,
+            usmePromoteDate: now, // create date default : now
+            usmePoints: dataUserMembers.usmePoints,
+            usmeType: 'default', // create default
+        }).then((result: any) => {
+            if (!result) {
+                throw new BadRequestException("Data insert failed");
+            }
+        }).catch((err: any) => {
+            return err;
+        });
+
+        // save data users.user_profiles
+        const saveUserProfiles = await this.userProfilesRepository.save({
+            usproUserId: idUser, // idUser : get by resultSaveUsers
+            usproNationalId: dataUserProfiles.usproNationalId,
+            usproBirth: dataUserProfiles.usproBirth,
+            usproJobTitle: dataUserProfiles.usproJobTitle,
+            usproMaritalStatus: dataUserProfiles.usproMaritalStatus,
+            usproGender: dataUserProfiles.usproGender,
+            usproAddr: dataUserProfiles.usproAddr
+        }).then((result: any) => {
+            if (!result) {
+                throw new BadRequestException("Data insert failed");
+            }
+        }).catch((err: any) => {
+            return err; 
+        });
+
+        
+    }
+
 }
