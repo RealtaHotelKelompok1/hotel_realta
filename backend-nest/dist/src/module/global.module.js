ede91e075d07ba6f723a27178b72f77110d266bf
Rifqi Ramdhani <rifqiramdhani71@yahoo.co.id>
2023-01-15 23:11:23 +0700

    add

--- backend-nest/dist/src/module/global.module.js
+++ backend-nest/dist/src/module/global.module.js
@@ -10,8 +10,11 @@ exports.GlobalModule = void 0;
 const common_1 = require("@nestjs/common");
 const typeorm_1 = require("@nestjs/typeorm");
 const Department_1 = require("../../entities/Department");
+const Shift_1 = require("../../entities/Shift");
 const department_controller_1 = require("../controller/humanresource/department/department.controller");
+const shift_controller_1 = require("../controller/humanresource/shift/shift.controller");
 const department_service_1 = require("../service/humanresource/department/department.service");
+const shift_service_1 = require("../service/humanresource/shift/shift.service");
 let GlobalModule = class GlobalModule {
 };
 GlobalModule = __decorate([
@@ -19,13 +22,16 @@ GlobalModule = __decorate([
         imports: [
             typeorm_1.TypeOrmModule.forFeature([
                 Department_1.Department,
+                Shift_1.Shift
             ]),
         ],
         controllers: [
             department_controller_1.DepartmentController,
+            shift_controller_1.ShiftController
         ],
         providers: [
             department_service_1.DepartmentService,
+            shift_service_1.ShiftService
         ],
     })
 ], GlobalModule);
