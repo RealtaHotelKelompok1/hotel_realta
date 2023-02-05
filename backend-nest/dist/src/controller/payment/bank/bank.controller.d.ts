ccf9c8367192ceba80ca79e18a67632f1dd7ef45
Rifqi Ramdhani <rifqiramdhani71@yahoo.co.id>
2023-02-05 13:49:53 +0700

    new commit

--- backend-nest/dist/src/controller/payment/bank/bank.controller.d.ts
+++ backend-nest/dist/src/controller/payment/bank/bank.controller.d.ts
@@ -5,8 +5,8 @@ export declare class BankController {
     private bankService;
     constructor(bankService: BankService);
     findAllBank(): Promise<import("../../../../entities/Bank").Bank | import("../../../../entities/Bank").Bank[]>;
-    findBankById(id: number): Promise<import("../../../../entities/Bank").Bank | import("../../../../entities/Bank").Bank[] | HttpException>;
-    updateBank(id: number, body: BankDto): Promise<import("../../../../entities/Bank").Bank | import("../../../../entities/Bank").Bank[] | HttpException>;
+    findBankById(id: number): Promise<import("../../../../entities/Bank").Bank | HttpException | import("../../../../entities/Bank").Bank[]>;
+    updateBank(id: number, body: BankDto): Promise<import("../../../../entities/Bank").Bank | HttpException | import("../../../../entities/Bank").Bank[]>;
     insertBank(body: BankDto): Promise<import("typeorm").InsertResult>;
     deleteBank(id: number): Promise<string>;
 }
