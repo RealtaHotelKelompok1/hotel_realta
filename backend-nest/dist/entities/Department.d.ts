94bb0c7439d745e9fc1f310be0af2cfe92059acc
annisann <ennoza@student.ub.ac.id>
2023-02-08 00:31:45 +0700

    delete dist.....

--- backend-nest/dist/entities/Department.d.ts
+++
@@ -1,7 +0,0 @@
-import { EmployeeDepartmentHistory } from "./EmployeeDepartmentHistory";
-export declare class Department {
-    deptId: number;
-    deptName: string | null;
-    deptModifiedDate: Date | null;
-    employeeDepartmentHistories: EmployeeDepartmentHistory[];
-}
--- backend-nest/dist/entities/Members.d.ts
+++
@@ -1,6 +0,0 @@
-import { UserMembers } from "./UserMembers";
-export declare class Members {
-    membName: string;
-    membDescription: string | null;
-    userMembers: UserMembers[];
-}
