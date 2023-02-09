732dc4bdfa519965a22645c72ff37c88ea96da31
Rifqi Ramdhani <rifqiramdhani71@yahoo.co.id>
2023-02-09 13:26:11 +0700

    new push commit

--- backend-nest/src/service/payment/user_accounts/user_accounts.service.ts
+++ backend-nest/src/service/payment/user_accounts/user_accounts.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserAccounts } from 'entities/UserAccounts';
 import { Repository } from 'typeorm';
-import { AccountType } from '../../../../../types/enums';
+// import { AccountType } from '../../../../';
 
 @Injectable()
 export class UserAccountsService {
