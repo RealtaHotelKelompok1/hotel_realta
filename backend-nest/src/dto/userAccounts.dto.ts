728b95c7083b480da55266be4a8a8db14d2bfcab
Annisa Nuri Nabila <73707695+annisann@users.noreply.github.com>
2023-02-09 13:14:09 +0700

    reemove setter and getter cuz it doesnt work

--- backend-nest/src/dto/userAccounts.dto.ts
+++ backend-nest/src/dto/userAccounts.dto.ts
@@ -1,11 +1,7 @@
 import { IsNotEmpty, IsString, IsOptional, MaxLength } from 'class-validator';
 
 export class UserAccountsDto {
-    private _cardHolderName: string | null = null;
-    private _entityName: string | null = null;
-    private _expMonth: any | null = null;
-    private _expYear: number | null = null;
-
+    
     @IsNotEmpty()
     userId: number;
 
@@ -14,43 +10,19 @@ export class UserAccountsDto {
     
     @IsNotEmpty()
     securedKey: string;
-    
-    @IsOptional()
-    set cardHolderName(value: string | null) {
-        this._cardHolderName = value === '' ? null : value;
-    }
-
-    get cardHolderName() {
-        return this._cardHolderName;
-    }
 
     @IsOptional()
-    set entityName(value: string | null) {
-        this._entityName = value === '' ? null : value;
-    }
+    cardHolderName: string;
 
-    get entityName() {
-        return this._entityName;
-    }
+    @IsOptional()
+    entityName: string;
 
     @IsOptional()
     accountNumber: string;
 
     @IsOptional()
-    set expMonth(value: any | null) {
-        this._expMonth = value === '' ? null : +value;
-    }
-
-    get expMonth() {
-        return this._expMonth;
-    }
+    expMonth: number;
 
     @IsOptional()
-    set expYear(value: any | null) {
-        this._expYear = value === '' ? null : +value;
-    }
-
-    get expYear() {
-        return this._expYear;
-    }
+    expYear: number;
 }
