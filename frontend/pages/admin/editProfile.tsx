c0f056caab755b5ed537d9dd9640a054e80da61a
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-02-09 09:18:43 +0700

    Update editProfile.tsx

--- frontend/pages/admin/editProfile.tsx
+++ frontend/pages/admin/editProfile.tsx
@@ -7,6 +7,9 @@ import { doUpdatePhotoUsers } from "@/redux/Actions/Users/reduceActions";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import LayoutAdmin from "@/components/Layout/LayoutAdmin";
+import { Box } from "@mui/material";
+import { ChevronUpIcon } from "@heroicons/react/24/solid";
+import { PhotoIcon } from "@heroicons/react/20/solid";
 
 interface Props {
   dirs: string[];
@@ -45,9 +48,11 @@ const EditProfile: NextPage<Props> = ({ dirs })=> {
     return (
       <>
         <LayoutAdmin>
-          <p className="text-gray-700 text-3xl mb-16 font-bold">Edit Profile</p>
-          <div className="grid col-1 bg-white shadow-sm">
-            <div className="max-w-4xl mx-auto p-20 space-y-6">
+          <p className="text-gray-700 text-3xl mb-16 font-bold">Profile</p>
+          <Box className="grid col-1 bg-white h-50 shadow-xl rounded-md">
+            <label className="pt-3 text-center font-bold">Profile Photo</label>  
+            <hr className="mt-3"/>
+            <Box className="max-w-4xl mx-auto pt-3 space-y-3 pb-3">
               <label>
                 <input
                   type="file"
@@ -60,31 +65,28 @@ const EditProfile: NextPage<Props> = ({ dirs })=> {
                     }
                   }}
                 />
-                <div className="w-40 aspect-video rounded flex items-center justify-center border-2 border-dashed cursor-pointer">
+                <Box className="w-40 aspect-video rounded flex items-center justify-center border-2 border-dashed cursor-pointer">
                   {selectedImage ? (
                     <img src={selectedImage} alt="" />
                   ) : (
                     <span>Select Photo</span>
                   )}
-                </div>
+                </Box>
               </label>
               <button
+                className="shadow-lg w-40 px-4 py-2 mx-auto rounded-md items-center bg-orange-100 text-center text-sm font-medium normal-case text-orange-900 hover:bg-orange-200 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-75"
                 onClick={handleUpload}
                 disabled={uploading}
-                style={{ opacity: uploading ? ".5" : "1" }}
-                className="bg-red-600 p-3 w-32 text-center rounded text-white"
-              >
-                {uploading ? "Uploading.." : "Upload"}
+                style={{ opacity: uploading ? ".5" : "1" }}>
+                  <p>{uploading ? "Uploading.." : "Upload"}</p>
               </button>
-              {/* <div className="mt-20 flex flex-col space-y-3">
-                {dirs.map((item) => (
-                  <Link key={item} href={"/images/" + item}>
-                    <label className="text-blue-500 hover:underline">{item}</label>
-                  </Link>
-                ))}
-              </div> */}
-            </div>
-          </div>
+            </Box>
+          </Box>
+          <Box className="grid mt-3 col-1 bg-white h-50 shadow-xl rounded-md">
+            <label className="pt-3 text-center font-bold">Profile Me</label>
+            <hr className="mt-3" />
+            <label className="text-center mt-3 mb-3 text-gray-400">On progress...</label>
+          </Box>
         </LayoutAdmin>
         </>
     )
