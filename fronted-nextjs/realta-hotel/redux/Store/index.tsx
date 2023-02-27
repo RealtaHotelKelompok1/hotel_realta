deb7d80af3dee1117afa4f9f76d25789b7598be1
Annisa Nuri Nabila <73707695+annisann@users.noreply.github.com>
2023-02-28 00:32:38 +0700

    redux-saga and constants/types for payment module

--- fronted-nextjs/realta-hotel/redux/Store/index.tsx
+++ fronted-nextjs/realta-hotel/redux/Store/index.tsx
@@ -3,13 +3,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from '../Sagas';
 import usersReducers from "../Reducers/Users/usersReducer";
+import bankReducer from "../Reducers/Payment/bank";
+import fintechReducer from "../Reducers/Payment/fintech";
+import transactionReducer from "../Reducers/Payment/transaction";
+import userAccountReducer from "../Reducers/Payment/userAccount";
 
 const saga = createSagaMiddleware()
 const reducer = combineReducers({
     //Master Reducer nya dibawah Comman masing" module
 
     //Users
-    usersReducers: usersReducers
+    usersReducers: usersReducers,
   
     //HR
 
@@ -20,6 +24,10 @@ const reducer = combineReducers({
     //Resto
 
     //Payment
+    transactionReducer,
+    bankReducer,
+    userAccountReducer,
+    fintechReducer
 
     //Purchase
 
