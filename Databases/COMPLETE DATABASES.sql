15e6e5ddbe9e17781e780ba27eeafe0bbce49128
Annisa Nuri Nabila <73707695+annisann@users.noreply.github.com>
2023-03-01 09:55:15 +0700

    update payment module tables

--- Databases/COMPLETE DATABASES.sql
+++ Databases/COMPLETE DATABASES.sql
@@ -430,7 +430,7 @@ create table booking.special_offer_coupons(
 -- ===========SCHEMA MODULE PAYMENT ==============
 CREATE SCHEMA payment;
 
-CREATE TABLE payment.entity (
+CREATE TABLE payment.entities (
 	entity_id				serial			PRIMARY KEY
 );
 
@@ -438,7 +438,7 @@ CREATE TABLE payment.bank (
 	bank_entity_id		  	int 		    PRIMARY KEY,
 	bank_code			  	varchar(10) 	UNIQUE,
 	bank_name			  	varchar(55) 	UNIQUE,
-	bank_modified_date		timestamp,
+	bank_modified_date		timestamp		DEFAULT NOW(),
 	CONSTRAINT bank_entity_id_fk
 		FOREIGN KEY (bank_entity_id) REFERENCES payment.entity(entity_id) ON UPDATE CASCADE
 );
@@ -447,7 +447,7 @@ CREATE TABLE payment.payment_gateaway (
 	paga_entity_id		  	int				PRIMARY KEY,
 	paga_code			  	varchar(10)		UNIQUE,
 	paga_name			  	varchar(55)		UNIQUE,
-	paga_modified_date		timestamp,
+	paga_modified_date		timestamp		DEFAULT NOW(),
 	CONSTRAINT paga_entity_id
 		FOREIGN KEY (paga_entity_id) REFERENCES payment.entity(entity_id) ON UPDATE CASCADE
 );
@@ -455,12 +455,12 @@ CREATE TABLE payment.payment_gateaway (
 CREATE TABLE payment.user_accounts (
 	usac_entity_id		    int,
 	usac_user_id		    int,
-	usac_account_number	  	varchar(25) UNIQUE,
+	usac_account_number	  	varchar(25) 	UNIQUE,
 	usac_saldo			    numeric,
 	usac_type			    varchar(15), -- debet | cc | payment
 	usac_expmonth		    smallint,
 	usac_expyear		    smallint,
-	usac_modified_date	  	timestamp,
+	usac_modified_date	  	timestamp 		DEFAULT NOW(),
 	usac_card_holder_name	varchar(50),
 	usac_secured_key		varchar,
 	CONSTRAINT user_accounts_pk
@@ -478,11 +478,11 @@ CREATE TABLE payment.payment_transaction (
 	patr_credit			numeric,
 	patr_type			char(3),
 	patr_note			varchar(255),
-	patr_modified_date	timestamp,
+	patr_modified_date	timestamp 		DEFAULT NOW(),
 	patr_order_number	varchar(55), 
 	patr_trx_number_ref	varchar(55) 	UNIQUE,
-	patr_source_id		int,
-	patr_target_id		int,
+	patr_source_id		numeric,
+	patr_target_id		numeric,
 	patr_user_id		int,
   CONSTRAINT patr_user_id_fk
 		FOREIGN KEY (patr_user_id) REFERENCES users.users(user_id) ON UPDATE CASCADE
